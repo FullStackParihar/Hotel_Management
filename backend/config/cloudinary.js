ea654479bdd60ee8af7ba6bcb5c8d5379f8ab626
Vishnu Parihar <75491313+FullStackParihar@users.noreply.github.com>
2025-05-05 01:13:02 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dwvt8pq94", // Replace with your Cloudinary cloud name
-  api_key: "216446426953217", // Replace with your API key
-  api_secret: "T9SbHOzVWLtT4kg0Y7EVYn2AK_4", // Replace with your API secret
+  cloud_name: "dwvt8pq94",
+  api_key: "216446426953217", 
+  api_secret: "T9SbHOzVWLtT4kg0Y7EVYn2AK_4", 
 });
 
 module.exports = cloudinary;
