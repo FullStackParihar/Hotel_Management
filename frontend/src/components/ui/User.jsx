9438c0597e4ba31da9551e80005cbd958c16900e
vishnu <vishnuparihar239925@gmail.com>
2025-05-04 19:15:55 +0530

    done updated

--- frontend/src/components/ui/User.jsx
+++ frontend/src/components/ui/User.jsx
@@ -1,28 +1,9 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import {
-  FaTasks,
-  FaUsers,
-  FaPlus,
-  FaSignOutAlt,
-  FaListAlt,
-  FaEdit,
-  FaTrashAlt,
-  FaCheckCircle,
-  FaChevronDown,
-  FaChevronUp,
-} from "react-icons/fa";
-import { motion, AnimatePresence } from "framer-motion";
+import React from "react";
+ 
 import UserPanel from "../User/UserPanel";
 
 const User = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-
-    navigate("/");
-  };
+  
 
   return (
 
