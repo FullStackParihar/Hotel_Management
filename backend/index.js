322cb0ca3eb96e8b8aa835ae99d7d8390bbdc1d9
vishnu <vishnuparihar239925@gmail.com>
2025-05-30 12:22:37 +0530

    report ganerate

--- backend/index.js
+++ backend/index.js
@@ -33,13 +33,12 @@ app.use(fileUpload());
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://localhost:27017/location-manager", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  
     });
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit process with failure
+    
   }
 };
 
